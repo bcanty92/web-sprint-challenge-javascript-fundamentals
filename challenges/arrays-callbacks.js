e1fbccade062d72773874e2b3e8c8ea1f050c330
Brittany Canty <bcanty92@gmail.comgit config --global user.email:bcanty92@gmail.com>
2020-07-20 19:53:33 -0500

    update

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -37,7 +37,7 @@ console.log(displayNames);
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
 */
-const lowCaseAnimalNames = [];
+
 const lowCaseAnimalNames = zooAnimals.map(function(items) {
   return items.animal_name.toLowerCase();
 });
@@ -50,7 +50,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+
 
 const lowPopulationAnimals = zooAnimals.filter(function(animal) {
 
@@ -65,7 +65,7 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const lowPopulationAnimals = [];
+
 
 const lowPopulationAnimals = zooAnimals.filter(function(animal) {
 
